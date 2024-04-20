bbf215e22ccc73035528010497c4569e9c7ca041
Yosrahabouria <132350298+Yosrahabouria@users.noreply.github.com>
2024-04-20 23:33:30 +0200

    Update 10-hbnb_filters.py

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-"""starts a Flask web application"""
+"""Starts a Flask web application"""
 
 from flask import Flask, render_template
-from models import *
 from models import storage
+from models.state import State
+from models.amenity import Amenity
 
 app = Flask(__name__)
 
 
 @app.route('/hbnb_filters', strict_slashes=False)
 def filters():
-    """def to display a HTML page like"""
-    states = storage.all("State").values()
-    amenities = storage.all("Amenity").values()
-    return render_template('10-hbnb_filters.html', states=states,)
+    """Definition to display an HTML page"""
+    states = storage.all(State).values()
+    amenities = storage.all(Amenity).values()
+    return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """defintion to close the storage"""
+    """Definition to close the storage"""
     storage.close()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000)
+    app.run(host='0.0.0.0', port='5000')
